ff82a64c6619f4e47ca4d36af3e2671c13d07f0c
Gurparkash Singh <if20b212@technikum-wien.at>
2022-02-23 18:45:31 +0100

    Initial commit

---
+++ app/src/main/java/at/technikum_wien/singh/mobilecodereview/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package at.technikum_wien.singh.mobilecodereview.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlueDeep = Color(0xFF2C2892)
+val BlueContrast = Color(0xFF8D8CE3)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
---
+++ app/src/main/java/at/technikum_wien/singh/mobilecodereview/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package at.technikum_wien.singh.mobilecodereview.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = BlueDeep,
+    primaryVariant = BlueContrast,
+    secondary = White
+)
+
+private val LightColorPalette = lightColors(
+    primary = BlueDeep,
+    primaryVariant = BlueContrast,
+    secondary = White
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun MobileCodeReviewTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
