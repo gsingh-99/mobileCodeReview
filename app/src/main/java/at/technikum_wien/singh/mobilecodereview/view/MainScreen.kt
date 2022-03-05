e29b989adbd091dfacecf8ef42f1a89372c79947
Gurparkash Singh <if20b212@technikum-wien.at>
2022-03-05 12:53:44 +0100

    pull request icon

--- app/src/main/java/at/technikum_wien/singh/mobilecodereview/view/MainScreen.kt
+++ app/src/main/java/at/technikum_wien/singh/mobilecodereview/view/MainScreen.kt
@@ -1,15 +1,13 @@
 package at.technikum_wien.singh.mobilecodereview.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,7 +25,7 @@ fun MainScreen() {
             Button(onClick = { /*TODO*/ },
                 modifier=Modifier.fillMaxWidth(),
                 colors=ButtonDefaults.buttonColors(backgroundColor = Color.White)) {
-                    Icon(Icons.Filled.Menu, contentDescription = "Repository")
+                Icon(painter= painterResource(R.drawable.outline_storage_24), contentDescription = "Repository")
                 Spacer(modifier = Modifier.width(12.dp))
                 Text(text = stringResource(R.string.home_repository), color=MaterialTheme.colors.primary, style = MaterialTheme.typography.button)
                 Spacer(modifier = Modifier.weight(1f))
@@ -35,6 +33,8 @@ fun MainScreen() {
             Button(onClick = { /*TODO*/ },
                 modifier=Modifier.fillMaxWidth(),
                 colors=ButtonDefaults.buttonColors(backgroundColor = Color.White)) {
+                Icon(painter= painterResource(R.drawable.ic_pull_request), modifier = Modifier.size(24.dp), contentDescription = "Repository")
+                Spacer(modifier = Modifier.width(12.dp))
                 Text(text = stringResource(R.string.home_pull_request), color=MaterialTheme.colors.primary, style = MaterialTheme.typography.button)
                 Spacer(modifier = Modifier.weight(1f))
             }
