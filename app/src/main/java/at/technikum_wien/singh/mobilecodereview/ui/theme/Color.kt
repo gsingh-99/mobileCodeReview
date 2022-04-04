b3a11bceb6006ef26bc2dbbae9ae91444f3ce53d
Gurparkash Singh <gurparkashsingh99@gmail.com>
2022-04-04 17:16:24 +0200

    better contrast

--- app/src/main/java/at/technikum_wien/singh/mobilecodereview/ui/theme/Color.kt
+++ app/src/main/java/at/technikum_wien/singh/mobilecodereview/ui/theme/Color.kt
@@ -3,7 +3,7 @@ package at.technikum_wien.singh.mobilecodereview.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val BlueDeep = Color(0xFF2C2892)
-val BlueContrast = Color(0xFF8D8CE3)
+val BlueContrast = Color(0xFF8887E3)
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
 val LimeGreen = Color(0xFFBCFBBC)
